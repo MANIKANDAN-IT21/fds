f52c7c1ce919bf43aaba6ee6f29bb0166cc6d268
manikamaraj <manikamaraj.k@gmail.com>
2025-05-28 22:55:17 +0530

    Add the Header component

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+  isLoggedIn = false;
+
+  login() {
+    this.isLoggedIn = true;
+  }
+
+  logout() {
+    this.isLoggedIn = false;
+  }
+
+}
